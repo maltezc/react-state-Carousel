dcaf86540247a72a90050011c14a8e16784dba62
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2023-01-17 16:58:07 -0800

    woring on carousel

--- carousel/src/Card.test.js
+++ carousel/src/Card.test.js
@@ -5,12 +5,12 @@ import Card from "./Card";
 describe("Card tests", function () {
   // smoke test
   test("smoke test", function () {
-    render(<Card caption="test caption" src="test caption" currNum={3} totalNum={3}/>)
+    render(<Card caption="test caption" src="test caption" currNum={3} totalNum={3} />)
   })
 
   // snapshot test
   test("matches snapshot", function () {
-    const {container} = render(<Card caption="test caption" src="test caption" currNum={3} totalNum={3}/>)
+    const { container } = render(<Card caption="test caption" src="test caption" currNum={3} totalNum={3} />)
     expect(container).toMatchSnapshot();
   })
 
--- carousel/src/Carousel.test.js
+++ carousel/src/Carousel.test.js
@@ -1,14 +1,29 @@
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 import TEST_IMAGES from "./_testCommon.js";
+import photos from "./photos.js";
 
-it("works when you click on the right arrow", function() {
+test("smoke test", function () {
+  render(<Carousel photos=' "src": "/static/media/image1.08e1519e765278b10548.jpg"'
+    title="test caption" />)
+})
+
+// snapshot test
+test("matches snapshot", function () {
+  const { container } = render(<Carousel photos=' "src": "/static/media/image1.08e1519e765278b10548.jpg"'
+    title="test caption" />)
+  expect(container).toMatchSnapshot();
+})
+
+it("works when you click on the right arrow", function () {
   const { container } = render(
     <Carousel
       photos={TEST_IMAGES}
       title="images for testing"
     />
+
   );
+
   // expect the first image to show, but not the second
   expect(
     container.querySelector('img[alt="testing image 1"]')
@@ -29,3 +44,22 @@ it("works when you click on the right arrow", function() {
     container.querySelector('img[alt="testing image 2"]')
   ).toBeInTheDocument();
 });
+
+
+test("demonstrate that leftArrow and rightArrow do the same thing", function () {
+  const { debug, container } = render(<Carousel photos={photos} title="testTitle" />)
+  debug(container)
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  const leftArrow = container.querySelector("bi bi-arrow-right-circle")
+  console.log(container.querySelector("img"))
+  fireEvent.click(rightArrow);
+
+
+  // fireEvent.click(leftArrow);
+  // const containerAfterleft = container
+
+  console.log("originalContainer", originalContainer)
+  console.log("containerAfterRight", containerAfterRight)
+
+})
+
